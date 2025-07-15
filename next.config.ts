8597630eec489f7f93ee427a31296bab7092a9d5
oofarhanafarhat <khnkaneez@gmail.com>
2025-07-16 00:30:45 +0500

    added changes in next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from 'next';
 
 /** @type {NextConfig} */
 const nextConfig: NextConfig = {
-  eslint: {
-    // build ke dauraan ESLint errors ko ignore karega
-    ignoreDuringBuilds: true,
-  },
+ 
 };
 
 export default nextConfig;
